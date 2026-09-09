3e55819f209726e5f090dc70eb5788302cf4f3a0
Paula Pajares Álvarez <pajaresalvarezp@gmail.com>
2026-09-09 14:36:13 +0200

    Revertir color de franja de valores

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -426,7 +426,7 @@ function Home() {
     <main>
       <Hero />
 
-      <section className="relative overflow-hidden bg-cloud">
+      <section className="relative overflow-hidden bg-[#abd4ff]">
         <svg
           aria-hidden
           className="absolute inset-x-0 top-0 h-8 w-full text-background sm:h-14"
